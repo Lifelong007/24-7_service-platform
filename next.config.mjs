ecea84a55b11da01f9b33c283fa72fd949bef76f
Lifelong007 <important_lifelong007@.gmail.com>
2025-06-14 08:34:21 +0300

    done 3

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,18 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // Yoki maxsus domen
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
